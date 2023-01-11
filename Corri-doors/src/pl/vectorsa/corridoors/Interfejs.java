742850da24a2c4a26c39bd0a1d55829e6bd2cf18
VectorSA <98692459+VectorSA@users.noreply.github.com>
2023-01-11 20:54:52 +0100

    Logika i Interfejs

--- Corri-doors/src/pl/vectorsa/corridoors/Interfejs.java
+++ Corri-doors/src/pl/vectorsa/corridoors/Interfejs.java
@@ -1,27 +1,151 @@
 package pl.vectorsa.corridoors;
+
 import javax.swing.JButton;
-import javax.swing.JComboBox;
+import java.awt.event.KeyListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.ActionListener;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import javax.imageio.ImageIO;
+import java.net.URL;
+import javax.swing.JOptionPane;
+import java.awt.image.BufferedImage;
 
+/**
+ * Klasa Rysuj - Panel rysowania mapy
+ */
+class Rysuj extends JPanel { 
+/**
+ *  Interfejs - aktywny interfejs użytkownika
+ */
+    Interfejs interfejs;
+/**
+ *  Rysuj - Przypisuje panel graficzny dla mapy w interfejsię użytkownika
+ */
+    Rysuj(Interfejs interfejs) {
+        super();
+        this.interfejs = interfejs;
+    }
+/**
+ * Paint - wrzucenie grafik na panel mapy
+ */
+    @Override
+    public void paint(Graphics g) {
+        super.paintComponent(g);
+        Graphics2D g2 = (Graphics2D) g;
+        String mapa[][];
+        if (this.interfejs.logika.poziom == this.interfejs.logika.ostatniPoziom)
+            mapa = this.interfejs.logika.mapy[this.interfejs.logika.ostatniPoziom-1];
+        else
+            mapa = this.interfejs.logika.mapy[this.interfejs.logika.poziom];
+        g2.setColor(Color.WHITE);
+        g2.fillRect(0, 0, 960, 960);
+        for (int i = 0; i < mapa.length; i++) {
+            for (int j = 0; j < mapa.length; j++) {
+                try {  // ---------------- wrzucenie grafik na panel mapy
+                    if (Integer.valueOf(mapa[i][j].substring(0,2).substring(0,2)) == 0) {
+                        g2.drawImage((BufferedImage)ImageIO.read(new URL(new Main().getClass().getResource("images/").toString()+"wall.png")), j*64, i*64, null);
+                    } else if (Integer.valueOf(mapa[i][j].substring(0,2)) == 1) {
+                        g2.drawImage((BufferedImage)ImageIO.read(new URL(new Main().getClass().getResource("images/").toString()+"floor.png")), j*64, i*64, null);
+                    } else if (Integer.valueOf(mapa[i][j].substring(0,2)) == 2) {
+                        g2.drawImage((BufferedImage)ImageIO.read(new URL(new Main().getClass().getResource("images/").toString()+"door.png")), j*64, i*64, null);
+                    } else if (Integer.valueOf(mapa[i][j].substring(0,2)) == 3) {
+                        g2.drawImage((BufferedImage)ImageIO.read(new URL(new Main().getClass().getResource("images/").toString()+"door_opened.png")), j*64, i*64, null);
+                    } else if (Integer.valueOf(mapa[i][j].substring(0,2)) == 4) {
+                        g2.drawImage((BufferedImage)ImageIO.read(new URL(new Main().getClass().getResource("images/").toString()+"key.png")), j*64, i*64, null);
+                    } else if (Integer.valueOf(mapa[i][j].substring(0,2)) == 5) {
+                        g2.drawImage((BufferedImage)ImageIO.read(new URL(new Main().getClass().getResource("images/").toString()+"key.png")), j*64, i*64, null);
+                    } else if (Integer.valueOf(mapa[i][j].substring(0,2)) == 6) {
+                        
+                        if (Integer.valueOf(mapa[i][j].substring(0,2).substring(0,2)) == 0) {
+                            g2.drawImage((BufferedImage)ImageIO.read(new URL(new Main().getClass().getResource("images/").toString()+"wall.png")), j*64, i*64, null);
+                        } else if (Integer.valueOf(this.interfejs.logika.ostatniElement.substring(0,2)) == 1) {
+                            g2.drawImage((BufferedImage)ImageIO.read(new URL(new Main().getClass().getResource("images/").toString()+"floor.png")), j*64, i*64, null);
+                        } else if (Integer.valueOf(this.interfejs.logika.ostatniElement.substring(0,2)) == 2) {
+                            g2.drawImage((BufferedImage)ImageIO.read(new URL(new Main().getClass().getResource("images/").toString()+"door.png")), j*64, i*64, null);
+                        } else if (Integer.valueOf(this.interfejs.logika.ostatniElement.substring(0,2)) == 3) {
+                            g2.drawImage((BufferedImage)ImageIO.read(new URL(new Main().getClass().getResource("images/").toString()+"door_opened.png")), j*64, i*64, null);
+                        } else if (Integer.valueOf(this.interfejs.logika.ostatniElement.substring(0,2)) == 4) {
+                            g2.drawImage((BufferedImage)ImageIO.read(new URL(new Main().getClass().getResource("images/").toString()+"key.png")), j*64, i*64, null);
+                        } else if (Integer.valueOf(this.interfejs.logika.ostatniElement.substring(0,2)) == 5) {
+                            g2.drawImage((BufferedImage)ImageIO.read(new URL(new Main().getClass().getResource("images/").toString()+"key.png")), j*64, i*64, null);
+                        } else if (Integer.valueOf(this.interfejs.logika.ostatniElement.substring(0,2)) == 7) {
+                            g2.drawImage((BufferedImage)ImageIO.read(new URL(new Main().getClass().getResource("images/").toString()+"lever.png")), j*64, i*64, null);
+                        } else if (Integer.valueOf(this.interfejs.logika.ostatniElement.substring(0,2)) == 8) {
+                            g2.drawImage((BufferedImage)ImageIO.read(new URL(new Main().getClass().getResource("images/").toString()+"lever_toggled.png")), j*64, i*64, null);
+                        } else if (Integer.valueOf(this.interfejs.logika.ostatniElement.substring(0,2)) == 9) {
+                            g2.drawImage((BufferedImage)ImageIO.read(new URL(new Main().getClass().getResource("images/").toString()+"exit.png")), j*64, i*64, null);
+                        } else if (Integer.valueOf(this.interfejs.logika.ostatniElement.substring(0,2)) == 10) {
+                            g2.drawImage((BufferedImage)ImageIO.read(new URL(new Main().getClass().getResource("images/").toString()+"lever.png")), j*64, i*64, null);
+                        } else if (Integer.valueOf(this.interfejs.logika.ostatniElement.substring(0,2)) == 11) {
+                            g2.drawImage((BufferedImage)ImageIO.read(new URL(new Main().getClass().getResource("images/").toString()+"door.png")), j*64, i*64, null);
+                        } else if (Integer.valueOf(this.interfejs.logika.ostatniElement.substring(0,2)) == 12) {
+                            g2.drawImage((BufferedImage)ImageIO.read(new URL(new Main().getClass().getResource("images/").toString()+"door_opened.png")), j*64, i*64, null);
+                        }
+                        g2.drawImage((BufferedImage)ImageIO.read(new URL(new Main().getClass().getResource("images/").toString()+"player.png")), j*64, i*64, null);
+                    } else if (Integer.valueOf(mapa[i][j].substring(0,2)) == 7) {
+                        g2.drawImage((BufferedImage)ImageIO.read(new URL(new Main().getClass().getResource("images/").toString()+"lever.png")), j*64, i*64, null);
+                    } else if (Integer.valueOf(mapa[i][j].substring(0,2)) == 8) {
+                        g2.drawImage((BufferedImage)ImageIO.read(new URL(new Main().getClass().getResource("images/").toString()+"lever_toggled.png")), j*64, i*64, null);
+                    } else if (Integer.valueOf(mapa[i][j].substring(0,2)) == 9) {
+                        g2.drawImage((BufferedImage)ImageIO.read(new URL(new Main().getClass().getResource("images/").toString()+"exit.png")), j*64, i*64, null);
+                    } else if (Integer.valueOf(mapa[i][j].substring(0,2)) == 10) {
+                        g2.drawImage((BufferedImage)ImageIO.read(new URL(new Main().getClass().getResource("images/").toString()+"lever.png")), j*64, i*64, null);
+                    } else if (Integer.valueOf(mapa[i][j].substring(0,2)) == 11) {
+                        g2.drawImage((BufferedImage)ImageIO.read(new URL(new Main().getClass().getResource("images/").toString()+"door.png")), j*64, i*64, null);
+                    } else if (Integer.valueOf(mapa[i][j].substring(0,2)) == 12) {
+                        g2.drawImage((BufferedImage)ImageIO.read(new URL(new Main().getClass().getResource("images/").toString()+"door_opened.png")), j*64, i*64, null);
+                    }
+                } catch (Exception e) {}
+            }
+        }
+    }
+}
+/**
+ *  Klasa Interfejs - aktywny interfejs użytkownika
+ */
 class Interfejs extends JFrame {
-    JPanel mapa;
-    JPanel guziory;
-    JButton exit;
-    JButton menu;
-    JButton restart;
+/**
+ *  Atrybut Rysuj mapa - przypisywanie nazwy mapa
+ */
+    Rysuj mapa; // ---------------- Panel rysowania mapy
+/**
+ *  JPanel guziory - Panel przycisków
+ */
+    JPanel guziory; // ---------------- panel przycisków
+/**
+ *  JButton exit - przycisk wyjścia
+ */
+    JButton exit; // ---------------- przycisk wyjścia
+/**
+ *  JButton menu - przycisk menu
+ */
+    JButton menu; // ---------------- przycisk menu
+/**
+ *  JButton restart - przycisk restart
+ */
+    JButton restart; // ---------------- przycisk restartu
+/**
+ *  Atrybut LogikaMagika - przypisywanie nazwy logika
+ */
+    LogikaMagika logika; // ---------------- logika gry
 
+/**
+ *  Interfejs - aktywny interfejs użytkownika
+ */
     Interfejs() {
         super("Corri-Doors");
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setSize(new Dimension(960,1000));
         this.setLayout(null);
         this.setResizable(false);
-        this.mapa = new JPanel();
+        this.logika = new LogikaMagika(this);
+        this.mapa = new Rysuj(this);
         this.mapa.setSize(960,960);
         this.mapa.setLocation(0,40);
         this.mapa.setOpaque(true);
@@ -34,9 +158,9 @@ class Interfejs extends JFrame {
         this.exit = new JButton("WYJŚCIE");
         this.menu = new JButton("MENU");
         this.restart = new JButton("RESTART");
-        this.exit.setSize( 90,25);
+        this.exit.setSize(90,25);
         this.exit.setLocation(850,5);
-        this.exit.addActionListener(new ActionListener(){
+        this.exit.addActionListener(new ActionListener(){ // --------- Na słuchiwanie klawiszy WASD
             public void actionPerformed(ActionEvent e) {
                 System.exit(0);
             }
@@ -45,17 +169,50 @@ class Interfejs extends JFrame {
         this.restart.setSize( 95,25);
         this.restart.setLocation(740,5);
         this.guziory.add(restart);
-        this.menu.setSize( 90,25);
+        this.menu.setSize(90,25);
         this.menu.setLocation(630,5);
+        // ------------------------------------------ Na słuchiwanie klawiszy WASD
+        this.mapa.addKeyListener(new KeyListener(){
+            public void keyTyped(KeyEvent e) {
+                if (logika.poziom == logika.ostatniPoziom) {
+                    mapa.repaint();
+                    JOptionPane.showMessageDialog(null, "Wygrałeś, jeśli chcesz zagrać ponownie, uruchom ponownie grę!");
+                    return;
+                }
+                if (Character.toLowerCase(e.getKeyChar()) == 'd') {
+                    logika.logika("prawo");
+                } else if (Character.toLowerCase(e.getKeyChar()) == 'a') {
+                    logika.logika("lewo");
+                } else if (Character.toLowerCase(e.getKeyChar()) == 'w') {
+                    logika.logika("gora");
+                } else if (Character.toLowerCase(e.getKeyChar()) == 's') {
+                    logika.logika("dol");
+                }
+                repaint();
+            }
+/**
+ *  keyPressed - klawisz wciśnięty
+ */
+            public void keyPressed(KeyEvent e) {} // ---------- klawisz wciśnięty
+/**
+ *  keyReleased - klawisz wyciśniety
+ */
+            public void keyReleased(KeyEvent e) {} // ---------- klawisz puszczony
+        });
         this.guziory.add(menu);
         this.guziory.setLayout(null);
         this.add(mapa);
         this.add(guziory);
-        
+        this.repaint();
+        this.restart.setVisible(false);
+        this.menu.setVisible(false);
+        this.menu.setFocusable(false);
+        this.guziory.setFocusable(false);
+        this.exit.setFocusable(false);
+        this.restart.setFocusable(false);
+        this.setFocusable(false);
+        this.mapa.setFocusable(true);
+        this.mapa.requestFocusInWindow();
         this.setVisible(true);
-
-
-
-
     }
 }
--- Corri-doors/src/pl/vectorsa/corridoors/LogikaMagika.java
+++ Corri-doors/src/pl/vectorsa/corridoors/LogikaMagika.java
@@ -1,9 +1,248 @@
 package pl.vectorsa.corridoors;
+import javax.swing.JOptionPane;
+import java.lang.Math;
 
-
+/**
+ * Klasa LogikaMagika - odpowiada za logikę gry
+ */
 class LogikaMagika{
-    private String mapy[][][]={{{}}};
-    String[][][] daPanMapy (){
-        return this.mapy;
+    /**
+     * "00"-ściana "01"-podłoga "02"-drzwi "03"-otwarte_drzwi "04"-pies "05"-klucz "06"-gracz "07"-dźwignia 
+     * "08"-przełączona_dźwignia "09"-wyjście "10"-jedzenie "11"-metalowe_drzwi "12"-otwarte_metalowe_drzwi 
+     */
+    String mapy[][][]={
+        {
+            {"00","00","00","00","00","00","00","00","00","00","00","00","00","00","00"},
+            {"00","00","00","02-3","05-3","06","07-1","11-1","00","00","00","00","00","00","00"},
+            {"00","00","00","01","00","10-2","00","00","01","01","00","00","00","00","00"},
+            {"00","00","00","01","00","04-2","00","00","00","01","00","00","00","00","00"},
+            {"00","00","00","01","00","00","00","00","00","01","00","00","00","00","00"},
+            {"00","00","00","01","00","00","00","00","00","01","00","00","00","00","00"},
+            {"00","00","00","01","00","00","00","00","00","01","00","00","00","00","00"},
+            {"00","00","00","01","00","01","01","00","00","01","09","00","00","00","00"},
+            {"00","00","00","01","00","00","01","00","00","01","00","00","00","00","00"},
+            {"00","00","00","01","00","00","01","00","00","01","00","00","00","00","00"},
+            {"00","00","00","01","01","01","01","01","01","01","00","00","00","00","00"},
+            {"00","00","00","00","00","00","00","00","00","00","00","00","00","00","00"},
+            {"00","00","00","00","00","00","00","00","00","00","00","00","00","00","00"},
+            {"00","00","00","00","00","00","00","00","00","00","00","00","00","00","00"},
+            {"00","00","00","00","00","00","00","00","00","00","00","00","00","00","00"},
+        },
+        {
+            {"00","00","00","00","00","00","00","00","00","00","00","00","00","00","00"},
+            {"00","00","00","00","00","06","00","00","00","00","00","00","00","00","00"},
+            {"00","00","00","01","01","01","01","01","01","01","00","00","00","00","00"},
+            {"00","00","00","01","00","00","00","00","00","01","00","00","00","00","00"},
+            {"00","00","00","01","00","00","00","00","00","01","00","00","00","00","00"},
+            {"00","00","00","01","00","00","01","01","00","01","00","00","00","00","00"},
+            {"00","00","00","01","00","00","01","00","00","01","00","00","00","00","00"},
+            {"00","00","00","01","00","00","01","00","00","01","09","00","00","00","00"},
+            {"00","00","00","01","00","00","01","00","00","01","00","00","00","00","00"},
+            {"00","00","00","01","00","00","01","00","00","01","00","00","00","00","00"},
+            {"00","00","00","01","01","01","01","01","01","01","00","00","00","00","00"},
+            {"00","00","00","00","00","00","00","00","00","00","00","00","00","00","00"},
+            {"00","00","00","00","00","00","00","00","00","00","00","00","00","00","00"},
+            {"00","00","00","00","00","00","00","00","00","00","00","00","00","00","00"},
+            {"00","00","00","00","00","00","00","00","00","00","00","00","00","00","00"},
+        },
+        {
+            {"00","00","00","00","00","00","00","00","00","00","00","00","00","00","00"},
+            {"00","00","00","00","00","06","00","00","00","00","00","00","00","00","00"},
+            {"00","00","00","01","01","01","01","01","01","01","00","00","00","00","00"},
+            {"00","00","00","01","00","00","01","00","00","01","00","00","00","00","00"},
+            {"00","00","00","01","00","00","01","00","00","01","00","00","00","00","00"},
+            {"00","00","00","01","00","00","01","00","00","01","00","00","00","00","00"},
+            {"00","00","00","01","00","00","01","00","00","01","00","00","00","00","00"},
+            {"00","00","00","01","00","00","01","00","00","01","09","00","00","00","00"},
+            {"00","00","00","01","00","00","01","00","00","01","00","00","00","00","00"},
+            {"00","00","00","01","00","00","01","00","00","01","00","00","00","00","00"},
+            {"00","00","00","01","01","01","01","01","01","01","00","00","00","00","00"},
+            {"00","00","00","00","00","00","00","00","00","00","00","00","00","00","00"},
+            {"00","00","00","00","00","00","00","00","00","00","00","00","00","00","00"},
+            {"00","00","00","00","00","00","00","00","00","00","00","00","00","00","00"},
+            {"00","00","00","00","00","00","00","00","00","00","00","00","00","00","00"},
+        },
+    };
+/**
+ *  Interfejs - aktywny interfejs użytkownika
+ */
+    Interfejs interfejs;
+    String ostatniElement;
+    int poziom;
+    int ostatniPoziom;
+
+    LogikaMagika(Interfejs interfejs) {
+        this.interfejs = interfejs;
+        this.poziom = 0;
+        this.ostatniPoziom = 3;
+        this.ostatniElement = "01";
+    }
+
+    void logika(String strona) {
+        int wektor[] = new int[2];
+        int graczxy[] = new int[2];
+        String mapy[][] = this.mapy[this.poziom];
+        boolean wyjdz = false;
+        for (int i = 0; i < mapy.length; i++) {
+            for (int j = 0; j < mapy.length; j++) {
+                if (mapy[i][j].startsWith("06")) {
+                    graczxy[0] = j;
+                    graczxy[1] = i;
+                    wyjdz = true;
+                    break;
+                }
+            }
+            if (wyjdz) {
+                break;
+            }
+        }
+        if (strona == "prawo") {
+            wektor[0]= 1;
+            wektor[1]= 0;
+        } else if (strona == "lewo") {
+            wektor[0]= -1;
+            wektor[1]= 0;        
+        } else if (strona == "gora") {
+            wektor[0]= 0;
+            wektor[1]= -1;
+        } else if (strona == "dol") {
+            wektor[0]= 0;
+            wektor[1]= 1;
+        } else {
+            return;
+        }
+        int vx = graczxy[0]+wektor[0];
+        int vy = graczxy[1]+wektor[1];
+        if (vx < 0 || vx >= this.mapy[0].length || vy < 0 || vy >= this.mapy[0].length) {
+            return;
+        }
+        String gracz[] = mapy[graczxy[1]][graczxy[0]].split("-", 0);
+        String obiekt[] = mapy[vy][vx].split("-", 0);
+        String calyGracz = mapy[graczxy[1]][graczxy[0]];
+        String calyObiekt = mapy[vy][vx];
+        if (Integer.valueOf(obiekt[0]) == 0 || Integer.valueOf(obiekt[0]) == 11) {
+            return;
+        } else if (Integer.valueOf(obiekt[0]) == 1) {
+        } else if (Integer.valueOf(obiekt[0]) == 2) {
+            boolean znalezione = false;
+            for (int i = 1; i<gracz.length; i++) {
+                for (int j = 1; j<obiekt.length; j++) {
+                    if (Integer.valueOf(gracz[i]) == Integer.valueOf(obiekt[j])) {
+                        znalezione = true;
+                        break;
+                    }
+                }
+                if (znalezione) {
+                    break;
+                }
+            }
+            if (!znalezione) {
+                return;
+            }
+                obiekt[0] = "03";
+            calyObiekt = "";
+            for (int i = 0; i<obiekt.length; i++) {
+                calyObiekt += obiekt[i];
+                if (i<obiekt.length-1) {
+                    calyObiekt += "-";
+                }
+            }
+            mapy[vy][vx] = calyObiekt;
+        } else if (Integer.valueOf(obiekt[0]) == 3 || Integer.valueOf(obiekt[0]) == 12) {
+        } else if (Integer.valueOf(obiekt[0]) == 4) {
+            for (int i = 1; i<gracz.length; i++) {
+                if (Integer.valueOf(gracz[i]) == Integer.valueOf(obiekt[1])) {
+                    mapy[vy][vx] = "01";
+                    calyGracz = "";
+                    for (int j = 0; j<gracz.length; j++) {
+                        if (i == j) {
+                            continue;
+                        }
+                        calyGracz += gracz[j];
+                        if (j<gracz.length-1) {
+                            calyGracz += "-";
+                        }
+                    }
+                    break;
+                 }
+            }
+            if (!mapy[vy][vx].startsWith("01")) {
+                return;
+            }
+        } else if (Integer.valueOf(obiekt[0]) == 5 || Integer.valueOf(obiekt[0]) == 10) {
+            calyGracz = "";
+            for (int j = 0; j<gracz.length; j++) {
+                calyGracz += gracz[j];
+                if (j<gracz.length-1) {
+                    calyGracz += "-";
+                }
+            }
+            if (obiekt.length > 1) {
+                calyGracz += "-";
+            }
+            for (int j = 1; j<obiekt.length; j++) {
+                calyGracz += obiekt[j];
+                if (j<obiekt.length-1) {
+                    calyGracz += "-";
+                }
+            }
+            mapy[vy][vx] = "01";
+        } else if (Integer.valueOf(obiekt[0]) == 7 || Integer.valueOf(obiekt[0]) == 8) {
+            for (int i = 1; i<obiekt.length; i++) {
+                for (int j = 0; j<mapy.length; j++) {
+                    for (int k = 0; k<mapy.length; k++) {
+                        if (mapy[j][k].startsWith("11") || mapy[j][k].startsWith("12")) {
+                            String tymczasowyObiekt = "";
+                            String podzielonyObiekt[] = mapy[j][k].split("-", 0);
+                            if (Integer.valueOf(mapy[j][k].substring(0,2)) == 11) {
+                                tymczasowyObiekt = "12";
+                            } else if (Integer.valueOf(mapy[j][k].substring(0,2)) == 12) {
+                                tymczasowyObiekt = "11";
+                            }
+                            for (int h = 1; h<podzielonyObiekt.length; h++) {
+                                tymczasowyObiekt += podzielonyObiekt[h];
+                                if (h<podzielonyObiekt.length-1) {
+                                    tymczasowyObiekt += "-";
+                                }
+                            }
+                            mapy[j][k] = tymczasowyObiekt;
+                        }
+                    }
+                }
+            }
+            if (this.ostatniElement.startsWith("11") || this.ostatniElement.startsWith("12")) {
+                String tymczasowyObiekt = "";
+                String podzielonyObiekt[] = this.ostatniElement.split("-", 0);
+                if (Integer.valueOf(this.ostatniElement.substring(0,2)) == 11) {
+                    tymczasowyObiekt = "12";
+                } else if (Integer.valueOf(this.ostatniElement.substring(0,2)) == 12) {
+                    tymczasowyObiekt = "11";
+                }
+                for (int i = 1; i<podzielonyObiekt.length; i++) {
+                    tymczasowyObiekt += podzielonyObiekt[i];
+                    if (i<podzielonyObiekt.length-1) {
+                        tymczasowyObiekt += "-";
+                    }
+                }
+                this.ostatniElement = tymczasowyObiekt;
+            }
+        } else if (Integer.valueOf(obiekt[0]) == 9) {
+            this.interfejs.mapa.repaint();
+            this.poziom++;
+            mapy[graczxy[1]][graczxy[0]] = this.ostatniElement;
+            if(poziom!=ostatniPoziom)
+                this.ostatniElement = "01";
+            mapy[vy][vx] = calyGracz;
+            if(poziom==ostatniPoziom) {
+                this.ostatniElement = mapy[vy][vx];
+                JOptionPane.showMessageDialog(null, "Wygrałeś, jeśli chcesz zagrać ponownie, uruchom ponownie grę!");
+            }
+            return;
+        }
+        mapy[graczxy[1]][graczxy[0]] = this.ostatniElement;
+        this.ostatniElement = mapy[vy][vx];
+        mapy[vy][vx] = calyGracz;
+        this.mapy[this.poziom] = mapy;
+        this.interfejs.mapa.repaint();
     }
 }
